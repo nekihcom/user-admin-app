76d91cd33c172506cbdf7b1139f5e5e509a9d088
nekihcom <kmmch.tech@gmail.com>
2024-11-25 22:15:20 +0900

    chakra ui install

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <>
-            <p>aaa</p>
+            <p>aaabbb</p>
         </>
     );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { ChakraProvider } from '@chakra-ui/react';
+import theme from './theme/theme';
+
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
     </React.StrictMode>
 );
 
