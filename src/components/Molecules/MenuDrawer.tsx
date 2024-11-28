78edcbf5f30d612779740b13babf7c089e91018e
nekihcom <kmmch.tech@gmail.com>
2024-11-28 22:55:09 +0900

    ヘッダー終わり

---
+++ src/components/Molecules/MenuDrawer.tsx
@@ -0,0 +1,28 @@
+import { FC, memo, ReactNode } from "react";
+import { Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay } from "@chakra-ui/react";
+
+type Props = {
+    isOpen: boolean;
+    onClose: () => void;
+    onClickHome : () => void;
+    onClickUserManagement : () => void;
+    onClickSetting : () => void;
+}
+
+export const MenuDrawer:FC<Props> = memo((props) => {
+    const {isOpen, onClose, onClickHome, onClickUserManagement, onClickSetting} = props;
+
+    return (
+        <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
+            <DrawerOverlay>
+                <DrawerContent>
+                    <DrawerBody p={0} bg="gray.100">
+                        <Button onClick={onClickHome} w="100%">TOP</Button>
+                        <Button onClick={onClickUserManagement} w="100%">ユーザー一覧</Button>
+                        <Button onClick={onClickSetting} w="100%">設定</Button>
+                    </DrawerBody>
+                </DrawerContent>
+            </DrawerOverlay>
+        </Drawer>
+    );
+})
--- src/components/organisms/layout/Header.tsx
+++ src/components/organisms/layout/Header.tsx
@@ -1,35 +1,35 @@
-import { HamburgerIcon } from "@chakra-ui/icons";
-import { Box, Button, Drawer, DrawerBody, DrawerContent, DrawerOverlay, Flex, Heading, IconButton, Link, useDisclosure } from "@chakra-ui/react";
-import { FC, memo } from "react";
+import { Box, Flex, Heading, Link, useDisclosure } from "@chakra-ui/react";
+import { FC, memo, useCallback } from "react";
+
+import { MenuIconButton } from "../../atoms/Button/MenuIconButton";
+import { MenuDrawer } from "../../Molecules/MenuDrawer";
+import { useHistory } from "react-router-dom";
+
 
 export const Header:FC = memo(() => {
     const { isOpen, onOpen, onClose} = useDisclosure();
+    const history = useHistory();
+
+    const onClickHome = useCallback(() => history.push("/home"), []);
+    const onClickUserManagement = useCallback(() => history.push("/home/user_management"), []);
+    const onClickSetting = useCallback(() => history.push("/home/setting"), []);
+
 
     return (
         <>
             <Flex as="nav" bg="teal.500" color="gray.50" align="center" justify="space-between" padding={{base:3, md:5}}>
-                <Flex align="center" as="a" mr={8} _hover={{cursor:"pointer"}}>
+                <Flex align="center" as="a" mr={8} _hover={{cursor:"pointer"}} onClick={onClickHome}>
                     <Heading as="h1" fontSize={{base:"md", md:"lg"}}>ユーザー管理アプリ</Heading>
                 </Flex>
                 <Flex align="center" fontSize="sm" flexGrow={2} display={{base:"none", md:"flex"}}>
                     <Box pr={4}>
-                        <Link>ユーザー一覧</Link>
-                        <Link>設定</Link>
+                        <Link onClick={onClickUserManagement}>ユーザー一覧</Link>
+                        <Link onClick={onClickSetting}>設定</Link>
                     </Box>
                 </Flex>
-                <IconButton aria-label={"メニューボタン"} icon={<HamburgerIcon />} size="sm" variant="unstyled" display={{base:"block", md:"none"}} onClick={onOpen} />
+                <MenuIconButton onOpen={onOpen}></MenuIconButton>
             </Flex>
-            <Drawer placement="left" size="xs" isOpen={isOpen} onClose={onClose}>
-                <DrawerOverlay>
-                    <DrawerContent>
-                        <DrawerBody p={0} bg="gray.100">
-                            <Button w="100%">TOP</Button>
-                            <Button w="100%">ユーザー一覧</Button>
-                            <Button w="100%">設定</Button>
-                        </DrawerBody>
-                    </DrawerContent>
-                </DrawerOverlay>
-            </Drawer>
+            <MenuDrawer isOpen={isOpen} onClose={onClose} onClickHome={onClickHome} onClickUserManagement={onClickUserManagement} onClickSetting={onClickSetting} ></MenuDrawer>
         </>
     );
 })
