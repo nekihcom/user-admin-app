fb111c8213973bfb01d9b2b6113e948474d04e33
nekihcom <kmmch.tech@gmail.com>
2024-11-28 23:00:34 +0900

    ログイン画面の雛形作成

--- src/components/pages/Login.tsx
+++ src/components/pages/Login.tsx
@@ -1,9 +1,19 @@
+import { Box, Button, Divider, Flex, Heading, Input, Stack } from "@chakra-ui/react";
 import { FC, memo } from "react";
 
 export const Login:FC = memo(() => {
     return (
         <>
-            <p>Login</p>
+            <Flex align="center" justify="center" height="100vh" bg={"gray.200"}>
+                <Box bg="white" w="sm" p={4} borderRadius="md" shadow={"md"}>
+                    <Heading as="h1" size={"lg"} textAlign={"center"}>ユーザー管理アプリ</Heading>
+                    <Divider my={4} />
+                    <Stack spacing={6} py={4} px={10}>
+                        <Input placeholder="ユーザーID" />
+                        <Button bg={"teal.400"} color={"white"} _hover={{opacity: 0.8}}>ログイン</Button>
+                    </Stack>
+                </Box>
+            </Flex>
         </>
     );
 })
