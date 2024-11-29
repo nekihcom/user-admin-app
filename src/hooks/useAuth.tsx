07c716df004e8036110d4b74527457ea1cff7e2e
nekihcom <kmmch.tech@gmail.com>
2024-11-29 11:00:34 +0900

    メッセージ機能実装

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -2,24 +2,27 @@ import axios from "axios";
 import { useCallback, useState } from "react"
 import { User } from "../types/api/user";
 import { useHistory } from "react-router-dom";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
     const history = useHistory();
+    const {showMessage} = useMessage();
 
     const [loading, setLoading] = useState(false);
+
     const login = useCallback((id: string) => {
         setLoading(true);
         axios.get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-            .then(async res => {
+            .then(res => {
                 if (res.data) {
+                    showMessage({title: "ログインしました", status:"success"})
                     history.push("/home");
                 } else {
-                    // error
-                    alert("User not found.")
+                    showMessage({title: "ユーザーが見つかりません", status:"error"})
                 }
-            }).catch(() => alert("cannot login"))
+            }).catch(() => showMessage({title: "ログインできません", status:"error"})        )
             .finally(() => setLoading(false));
-    }, []);
+    }, [history, showMessage]);
 
     return {login, loading}
 }
